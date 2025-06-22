fde6ff253404dd096186e390846212a0f7eb9b60
Filpus <fil-pus@wp.pl>
2025-06-22 19:28:57 +0200

    All ready

--- Assets/Scripts/MusicSystem.cs
+++ Assets/Scripts/MusicSystem.cs
@@ -7,10 +7,12 @@ public class MusicManager : MonoBehaviour
 
     private AudioSource _audioSource;
 
-    [SerializeField] private AudioClipListSO playlist;
+    [SerializeField] private AudioClipListSO menuList;
+    [SerializeField] private AudioClipListSO levelList;
     void Start()
     {
 
+        _audioSource = GetComponent<AudioSource>();
         if (Instance == null)
         {
             MusicManager.Instance = this;
@@ -20,7 +22,21 @@ public class MusicManager : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+        PlayMenuMusic();
     }
 
+
+    public void PlayMenuMusic()
+    {
+        _audioSource.clip = menuList.TakeRandom();
+        _audioSource.Play();
+    }
     
+    
+    public void PlayLevelMusic()
+    {
+        _audioSource.clip = levelList.TakeRandom();
+        _audioSource.Play();
+    }
+
 }
