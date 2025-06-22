9454901b172d82502650b093d5e502f281201762
Akmetari <77680749+Akmetari@users.noreply.github.com>
2025-06-22 18:24:52 +0200

    intro monologue

--- Assets/Scripts/DialogDataSO.cs
+++ Assets/Scripts/DialogDataSO.cs
@@ -8,9 +8,11 @@ public class DialogDataSO : ScriptableObject
     public Dictionary<string, List<string>> dialogueDict = new Dictionary<string, List<string>>(){
         { "intro", new List<string>()
             {
-                "Witaj, bohaterze.",
-                "Zanim wyruszysz, musisz coś wiedzieć.",
-                "Ta kraina jest pełna niebezpieczeństw."
+                "Evil mage’s castle... It used to be my father’s castle.\r\nBefore that traitor poisoned our guards, killed my mother and brothers in their sleep, and finally thrust a dagger through my father’s heart.",
+                "I wasn't in my chamber that night — I don't even remember exactly why. Some stupid, childish tantrum made me run into the woods, where I got lost.\r\nI couldn't find my way back — but neither could they find me.",
+                "When I finally returned to the city in the morning, all I heard was mourning.\r\nThe coup went unpunished. People were too terrified of the evil forces backing the traitorous mage to protest.Soldiers either pledged loyalty to the new king or were executed.",
+                "The traitor ruled freely for almost ten years, but he won't see his 10th coronation anniversary. Tonight is the night I get my revenge. No matter the cost, even if the price is my life.",
+                "The traitor is a dead man walking."
             }
         } 
     };
